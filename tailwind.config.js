b0cd908f52f9500b4cefc3d28818e3fca14e6bb1
Alex Nascimento <alexjose68@hotmail.com>
2024-07-07 20:32:33 -0300

    feat: instalei o react-icons para usar alguns ícones no projeto e passei umas configurações necessárias que irei usar no decorrer do projeto no arquivo de configuração do tailwindcss, utilidades como fontes, cores e altura.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'screen-minus-header': 'calc(100vh - 64px)',
+      },
+      colors: {
+        primary: '#465975',
+        secondary: '#7B8CA6',
+        tertiary: '#E0E1DD',
+        beltColor: '#1D2639',
+        beautyNoir: "#232323",
+        beautyBrunette: "#343434"
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        gothic: ['Gothic A1', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
