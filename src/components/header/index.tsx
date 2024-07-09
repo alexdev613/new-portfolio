7fe3c58d31e5b8f476488eacae2d4e22a1b8f1a5
Alex Nascimento <alexjose68@hotmail.com>
2024-07-09 19:55:23 -0300

    style: mudança da cor de background do Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export function Header() {
   return (
     <div>
-      <header className='bg-beltColor h-16 '>
+      <header className='bg-black/90 h-16 '>
         <nav className='flex justify-around font-medium text-tertiary w-full max-w-screen-xl mx-auto pt-4 shadow-black'>
           <Link
             to="/"
